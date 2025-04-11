3dbd16be213831f6d28bfbb0f7b89c7266fdcb40
smaizeal25 <samizeal25@gmail.com>
2025-04-11 22:30:51 +0100

    Add project files.

---
+++ Jwt_OAuth_Security_For_API/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+
+namespace Jwt_OAuth_Security_For_API.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync ()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.UserName == "admin" && Credential.Password == "admin")
+                {
+                   
+
+                   var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, ""),
+                        new Claim(ClaimTypes.Email,""),
+                      
+                    };
+                    var claimsIdentity = new ClaimsIdentity(claims, "MyCookiesAuth");
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                    // Sign in the user
+                    await HttpContext.SignInAsync( "MyCookiesAuth", claimsPrincipal);
+                    Response.Redirect("/Index");
+                }
+                return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required, Display(Name ="User name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
