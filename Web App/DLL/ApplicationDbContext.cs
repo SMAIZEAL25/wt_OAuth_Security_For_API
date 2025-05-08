f252a77b137d937f5efa6c4b832446af54e21633
smaizeal25 <samizeal25@gmail.com>
2025-05-08 12:59:08 +0100

    web app

---
+++ Web App/DLL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_App.DLL
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+      
+    }
+    
+    
+}
